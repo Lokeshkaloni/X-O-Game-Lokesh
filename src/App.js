9c7e455a61c787655cc1449242ee320927084ecb
lokesh kaloni <lokeshkaloni@gmail.com>
2022-08-06 22:28:03 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import Board from "./Board";
 function App() {
-  const [reset, setReset] = useState(false);
-
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
       <h1 className="font-bold text-2xl">Lokesh X-O Game</h1>
