b4dc635bc5fb0c5822978f48578abcb1d78091e8
lokesh kaloni <lokeshkaloni@gmail.com>
2022-08-10 12:13:36 +0530

    adsense

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Board from "./Board";
 function App() {
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8296104314843679"
+        crossorigin="anonymous"
+      ></script>
       <h1 className="font-bold text-2xl">Lokesh X-O Game</h1>
       <div className="">
         <Board />
