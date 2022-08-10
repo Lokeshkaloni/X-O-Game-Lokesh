106e42fd27fe55772fdfaf0430a9264a8cc4e40f
lokesh kaloni <lokeshkaloni@gmail.com>
2022-08-10 12:22:08 +0530

    adsense script

--- src/index.js
+++ src/index.js
@@ -7,5 +7,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8296104314843679"
+      crossorigin="anonymous"
+    ></script>
   </React.StrictMode>
 );
