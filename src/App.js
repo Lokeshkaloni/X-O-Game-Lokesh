d45fd66671b9fbef2ddbfc38cd5e57d04d6c5760
lokesh kaloni <lokeshkaloni@gmail.com>
2022-08-18 11:11:59 +0530

    embeded video added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import Board from "./Board";
 function App() {
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/2-e1k87CQxc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
       <h1 className="font-bold text-2xl">Lokesh X-O Game</h1>
       <div className="">
         <Board />
